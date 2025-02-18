6d8787123612421f8f15cacd52e8d7df78c077c8
Sarthak Khandelwal <mastermind.sarthak@gmail.com>
2025-02-17 20:51:39 -0500

    add: give memory to host

--- wasm-env/src/lib.rs
+++ wasm-env/src/lib.rs
@@ -1,11 +1,35 @@
 use std::slice;
 use std::str;
+use std::alloc::{alloc, dealloc, Layout};
+use serde::{Serialize, Deserialize};
+use std::collections::HashMap;
+
+#[derive(Serialize, Deserialize)]
+pub struct Example {
+    pub field1: HashMap<u32, String>,
+    pub field2: Vec<Vec<f32>>,
+    pub field3: [f32; 4],
+}
 
 extern "C" {
     fn double(x: i32) -> i32;
     fn log_str(ptr: i32, len: i32);
+    fn log_struct(ptr: i32, len: i32);
 }
 
+#[no_mangle]
+pub extern "C" fn allocate(size: usize) -> *mut u8 {
+    let layout = Layout::from_size_align(size, 1).unwrap();
+    unsafe { alloc(layout) }
+}
+
+#[no_mangle]
+pub extern "C" fn deallocate(ptr: *mut u8, size: usize) {
+    if !ptr.is_null() {
+        let layout = Layout::from_size_align(size, 1).unwrap();
+        unsafe { dealloc(ptr, layout) }
+    }
+}
 
 #[no_mangle]
 pub extern fn add(left: i32, right: i32) -> i32 {
@@ -29,6 +53,23 @@ pub extern fn say_hello(ptr: i32, len: i32) {
     let out_str = format!("Hola, {}!", string_from_host);
     unsafe {
         log_str(out_str.as_ptr() as i32, out_str.len() as i32);
+    }
+}
+
+#[no_mangle]
+pub fn send_example_to_host() {
+    let mut field1 = HashMap::new();
+    field1.insert(0, String::from("ex"));
+    field1.insert(1, String::from("ex-1"));
+    let example = Example {
+        field1,
+        field2: vec![vec![1., 2.], vec![3., 4.]],
+        field3: [1., 2., 3., 4.]
+    };
+
+    let example_json = serde_json::to_string(&example).unwrap();
+    unsafe {
+        log_struct(example_json.as_ptr() as i32, example_json.len() as i32);
     }
 }
 
--- wasmtime_test/src/main.rs
+++ wasmtime_test/src/main.rs
@@ -1,7 +1,15 @@
 use std::error::Error;
 use wasmtime::*;
 use std::str;
+use serde::{Serialize, Deserialize};
+use std::collections::HashMap;
 
+#[derive(Serialize, Deserialize)]
+pub struct Example {
+    pub field1: HashMap<u32, String>,
+    pub field2: Vec<Vec<f32>>,
+    pub field3: [f32; 4],
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let engine = Engine::default();
@@ -10,6 +18,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut linker = Linker::new(&engine);
     linker.func_wrap("env", "double", double)?;
     linker.func_wrap("env", "log_str", log_str)?;
+    linker.func_wrap("env", "log_struct", log_struct)?;
 
     let mut store = Store::new(&engine, ());
     let instance = linker.instantiate(&mut store, &module)?;
@@ -19,6 +28,14 @@ fn main() -> Result<(), Box<dyn Error>> {
     
     println!("Memory data size: {:?}", memory.data_size(&store));
     println!("Memory size: {:?}", memory.size(&store));   
+
+    let alloc = instance.get_func(&mut store, "allocate")
+        .expect("`alloc` was not an exported function");
+    let alloc = alloc.typed::<i32, i32>(&store)?;
+    let curr_ptr = alloc.call(&mut store, 128)?;
+    println!("Allocated memory at: {:?}", curr_ptr);
+    let curr_ptr2 = alloc.call(&mut store, 128)?;
+    println!("Allocated memory at: {:?}", curr_ptr2);
 
     let add = instance.get_func(&mut store, "add")
         .expect("`add` was not an exported function");
@@ -38,11 +55,16 @@ fn main() -> Result<(), Box<dyn Error>> {
     
     let first_name = b"Sarthak";
     let last_name = b"Khandelwal";
-    memory.write(&mut store, 0, first_name)?;
+    memory.write(&mut store, curr_ptr.try_into().unwrap(), first_name)?;
     memory.write(&mut store, first_name.len(), last_name)?;
     
-    say_hello.call(&mut store, (0, first_name.len() as i32))?;
+    say_hello.call(&mut store, (curr_ptr.try_into().unwrap(), first_name.len() as i32))?;
     say_hello.call(&mut store, (first_name.len() as i32, last_name.len() as i32))?;
+
+    let send_example_to_host = instance.get_func(&mut store, "send_example_to_host")
+        .expect("`send_example_to_host` was not an exported function");
+    let send_example_to_host = send_example_to_host.typed::<(), ()>(&store)?;
+    send_example_to_host.call(&mut store, ())?;
     Ok(())
 }
 
@@ -69,4 +91,28 @@ fn log_str(mut caller: Caller<'_, ()>, ptr: i32, len: i32) {
         None => "pointer/length out of bounds",
     };
     println!("From wasm: {}", string);
+}
+
+fn log_struct(mut caller: Caller<'_, ()>, ptr: i32, len: i32) {
+    let mem = match caller.get_export("memory") {
+        Some(Extern::Memory(mem)) => mem,
+        _ => {
+            println!("failed to find `memory` export");
+            return;
+        },
+    };
+    let data = mem.data(&caller)
+        .get(ptr as u32 as usize..)
+        .and_then(|arr| arr.get(..len as u32 as usize));
+    let serialized = match data {
+        Some(data) => match str::from_utf8(data) {
+            Ok(s) => s,
+            Err(_) => "invalid utf-8",
+        },
+        None => "pointer/length out of bounds",
+    };
+    let deserialized: Example = serde_json::from_str(&serialized).unwrap();
+    println!("deserialized = {:?}", deserialized.field1);
+    println!("deserialized = {:?}", deserialized.field2);
+    println!("deserialized = {:?}", deserialized.field3);
 }
